aac349dee1dd0778392c872d455b46731a3eebc1
KKami91 <ghkdth919@gmail.com>
2024-05-17 15:30:11 +0900

    Fix region

--- busan-univ-project-interface-v1/pages/recharts_aws/index.tsx
+++ busan-univ-project-interface-v1/pages/recharts_aws/index.tsx
@@ -9,7 +9,7 @@ import { BubbleChat } from 'flowise-embed-react';
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+  region: 'ap-northeast-2',
 });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
