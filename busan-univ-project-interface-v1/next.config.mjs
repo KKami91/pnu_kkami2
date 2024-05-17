489d677d70e29cf1f96e70866ae757f77a381e86
KKami91 <85279584+KKami91@users.noreply.github.com>
2024-05-17 15:38:51 +0900

    Update next.config.mjs

--- busan-univ-project-interface-v1/next.config.mjs
+++ busan-univ-project-interface-v1/next.config.mjs
@@ -13,7 +13,8 @@ const nextConfig = {
   env: {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_REGION: process.env.AWS_REGION
+    AWS_REGION: process.env.AWS_REGION,
+    AWS_SDK_LOAD_CONFIG: "1",
   },
 };
 
