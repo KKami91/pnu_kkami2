fa22ae45ffae72f15e85c282d0b67c8f6355bc52
KKami91 <ghkdth919@gmail.com>
2024-05-17 15:48:12 +0900

    Fix region

--- busan-univ-project-interface-v1/next.config.mjs
+++ busan-univ-project-interface-v1/next.config.mjs
@@ -13,7 +13,6 @@ const nextConfig = {
   env: {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_REGION: process.env.AWS_REGION
   },
 };
 
