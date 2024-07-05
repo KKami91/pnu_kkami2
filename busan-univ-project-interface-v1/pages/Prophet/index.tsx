12027a059e8bffdf1527d3a507e7b54b9f282b5b
KKami91 <ghkdth919@gmail.com>
2024-07-05 12:16:00 +0900

    Update index.tsx

--- busan-univ-project-interface-v1/pages/Prophet/index.tsx
+++ busan-univ-project-interface-v1/pages/Prophet/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import axios from 'axios'
 
-const users = ['U#hwschaos@gmail.com', 'U#subak63@gmail.com']
+const users = ['U#hswchaos@gmail.com', 'U#subak63@gmail.com']
 
 export default function Home() {
   const [selectedUser, setSelectedUser] = useState('')
