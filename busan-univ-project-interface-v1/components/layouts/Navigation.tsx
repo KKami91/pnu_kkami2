55fc619f4a926b8fbc757d3b46361b6a53ca658e
KKami91 <ghkdth919@gmail.com>
2024-07-10 15:21:12 +0900

    Update Navigation.tsx

--- busan-univ-project-interface-v1/components/layouts/Navigation.tsx
+++ busan-univ-project-interface-v1/components/layouts/Navigation.tsx
@@ -21,7 +21,7 @@ const navigation = [
         id: "000",
         icon: PresentationChartLineIcon,
         name: "HRV Analysis",
-        href: "/neurokit",
+        href: "/analysis",
         tag: "",
       },
       {
