a5f684ccbc47932a83c982c2def6ef43157dd702
KKami91 <ghkdth919@gmail.com>
2024-08-19 15:46:26 +0900

    인원 추가

--- busan-univ-project-interface-v1/pages/analy/index.tsx
+++ busan-univ-project-interface-v1/pages/analy/index.tsx
@@ -6,7 +6,7 @@ import { SkeletonLoader } from '../../components/SkeletonLoaders3';
 import { LaptopMinimal, LayoutGrid } from 'lucide-react';
 import { parseISO, format, startOfHour, endOfHour } from 'date-fns';
 
-const users = ['hswchaos@gmail.com', 'subak63@gmail.com']
+const users = ['hswchaos@gmail.com', 'subak63@gmail.com', '27hyobin@gmail.com', 'skdlove1009@gmail.com']
 const API_URL = 'https://heart-rate-app10-hotofhe3yq-du.a.run.app'
 
 const LoadingSpinner = () => (
