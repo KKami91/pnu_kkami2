b4fa52055b2af19bb56058d435b2449e103c5a41
KKami91 <ghkdth919@gmail.com>
2024-09-11 15:56:54 +0900

    Update _app.tsx

--- busan-univ-project-interface-v1/pages/_app.tsx
+++ busan-univ-project-interface-v1/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "../styles/globals.css";
-
 import { ReactElement, ReactNode } from "react";
 import type { AppProps } from "next/app";
 import { NextPage } from "next";
+import { useRouter } from 'next/router';
 
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -26,16 +26,15 @@ type AppPropsWithLayout = AppProps & {
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const isHeatmapCharts = Component.name === 'HeatmapCharts';
-    // HeatmapCharts 페이지일 경우 레이아웃을 적용하지 않습니다.
+  const router = useRouter();
+
+  // 라우트 경로를 사용하여 HeatmapCharts 페이지인지 확인
+  const isHeatmapCharts = router.pathname === '/heatmap-charts';
+
+  // HeatmapCharts 페이지일 경우 레이아웃을 적용하지 않음
   const getLayout = isHeatmapCharts
     ? (page: ReactElement) => page
     : Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
-
-  // --- 기존 --- 
-  // const getLayout = Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
-
-  
 
   return (
     <RecoilRoot>
@@ -43,6 +42,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {getLayout(<Component {...pageProps} />)}
         </ThemeProvider>
+        {/* BubbleChat은 HeatmapCharts 페이지가 아닐 때만 렌더링 */}
         {!isHeatmapCharts && (
           <BubbleChat
             chatflowid="1352afdb-1933-4a3f-88ea-b55d560ea805"
